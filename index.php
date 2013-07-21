ff15c6a08e77879c85588cf544f2f91d234e654b
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2013-07-21 11:07:56 +0530

    Added social plugins

--- index.php
+++ index.php
@@ -27,6 +27,16 @@
 </head>
 
 <body>
+<!-- AddThis Button BEGIN -->
+<div class="addthis_toolbox addthis_floating_style addthis_32x32_style" style="left:0px;top:200px;background-color:#555555;">
+<a class="addthis_button_facebook"></a>
+<a class="addthis_button_twitter"></a>
+<a class="addthis_button_pinterest_share"></a>
+<a class="addthis_button_google_plusone_share"></a>
+<a class="addthis_button_compact"></a>
+</div>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=undefined"></script>
+<!-- AddThis Button END -->
     <header>
         <div class="container">
             <h1>
